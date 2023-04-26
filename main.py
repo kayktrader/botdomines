5ca028d17c99f1a38551c8b26e2ad0b906febce2
Raian Barbosa <57734572+raianb-dev@users.noreply.github.com>
2023-04-26 16:25:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ key_api = "5992692637:AAFNI31G_CnY06FPfsDpoT9oz5rfCiWRB8Y"
 bot = telebot.TeleBot(key_api)
 tz = pytz.timezone('America/Sao_Paulo')
 link_cadastro = "<a href='https://go.aff.estrelabetpartners.com/tf7n0i06'> Cadastre aqui</a>"
-link_game = "<a href='https://go.aff.estrelabetpartners.com/tf7n0i06'> Jogar agora</a>"
+link_game = "<a href='https://estrelabet.com/ptb/games/casino/detail/normal/7791'> Jogar agora</a>"
 def gerar_matriz():
     n_minas = random.choice([2, 3, 4, 5])
     if n_minas == 2:
